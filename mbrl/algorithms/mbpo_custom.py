592715c9de3431efb75a84e395c1dd35358902ea
Robert McCarthy <robertmccarthy97@gmail.com>
2023-06-08 15:41:07 +0100

    add custom training code

---
+++ mbrl/algorithms/mbpo_custom.py
@@ -0,0 +1,356 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+import os
+from typing import Optional, Sequence, cast
+
+import gymnasium as gym
+import hydra.utils
+import numpy as np
+import omegaconf
+import torch
+
+import mbrl.constants
+import mbrl.models
+import mbrl.planning
+import mbrl.third_party.pytorch_sac_pranz24 as pytorch_sac_pranz24
+import mbrl.types
+import mbrl.util
+import mbrl.util.common
+import mbrl.util.math
+from mbrl.planning.sac_wrapper import SACAgent
+from mbrl.third_party.pytorch_sac import VideoRecorder
+
+from mbrl.util.custom_utils import VideoRecorder, CustomLogHandler
+
+MBPO_LOG_FORMAT = mbrl.constants.EVAL_LOG_FORMAT + [
+    ("epoch", "E", "int"),
+    ("rollout_length", "RL", "int"),
+]
+
+"""
+# TODO:
+- Norm agent observations
+
+"""
+
+def rollout_model_and_populate_sac_buffer(
+    model_env: mbrl.models.ModelEnv,
+    replay_buffer: mbrl.util.ReplayBuffer,
+    agent: SACAgent,
+    sac_buffer: mbrl.util.ReplayBuffer,
+    sac_samples_action: bool,
+    rollout_horizon: int,
+    batch_size: int,
+    env_steps: int,
+    custom_log_handler: CustomLogHandler,
+):
+    # stats
+    obs_list, reward_list, action_list = [], [], []
+    imag_steps = 0
+    assert rollout_horizon > 0
+    # sample
+    batch = replay_buffer.sample(batch_size)
+    initial_obs, *_ = cast(mbrl.types.TransitionBatch, batch).astuple()
+    model_state = model_env.reset(
+        initial_obs_batch=cast(np.ndarray, initial_obs),
+        return_as_np=True,
+    )
+    accum_dones = np.zeros(initial_obs.shape[0], dtype=bool)
+    obs = initial_obs
+    for i in range(rollout_horizon):
+        action = agent.act(obs, sample=sac_samples_action, batched=True)
+        pred_next_obs, pred_rewards, pred_dones, model_state = model_env.step(
+            action, model_state, sample=True
+        )
+        truncateds = np.zeros_like(pred_dones, dtype=bool)
+        sac_buffer.add_batch(
+            obs[~accum_dones],
+            action[~accum_dones],
+            pred_next_obs[~accum_dones],
+            pred_rewards[~accum_dones, 0],
+            pred_dones[~accum_dones, 0],
+            truncateds[~accum_dones, 0],
+        )
+        # stats
+        obs_list.append(obs.copy()), reward_list.append(pred_rewards.copy()), action_list.append(action.copy())
+        imag_steps += pred_next_obs.shape[0]
+        # next
+        obs = pred_next_obs
+        accum_dones |= pred_dones.squeeze()
+    # stats
+    obs_list.append(obs.copy())
+    custom_log_handler.log_rollout(env_steps, obs_list, reward_list, action_list, type="rollout_imag", imag_steps=imag_steps)
+
+
+def evaluate(
+    env: gym.Env,
+    agent: SACAgent,
+    num_episodes: int,
+    video_recorder: VideoRecorder,
+) -> float:
+    avg_episode_reward = 0.0
+    avg_success = 0.0
+    for episode in range(num_episodes):
+        obs, _ = env.reset()
+        video_recorder.init(enabled=(episode == 0))
+        terminated = False
+        truncated = False
+        episode_reward = 0.0
+        while not terminated and not truncated:
+            action = agent.act(obs)
+            obs, reward, terminated, truncated, _ = env.step(action)
+            video_recorder.record(env)
+            episode_reward += reward
+        avg_episode_reward += episode_reward
+        avg_success += (reward == 0.0)
+    return avg_episode_reward / num_episodes, avg_success / num_episodes
+
+
+def maybe_replace_sac_buffer(
+    sac_buffer: Optional[mbrl.util.ReplayBuffer],
+    obs_shape: Sequence[int],
+    act_shape: Sequence[int],
+    new_capacity: int,
+    seed: int,
+) -> mbrl.util.ReplayBuffer:
+    if sac_buffer is None or new_capacity != sac_buffer.capacity:
+        if sac_buffer is None:
+            rng = np.random.default_rng(seed=seed)
+        else:
+            rng = sac_buffer.rng
+        new_buffer = mbrl.util.ReplayBuffer(new_capacity, obs_shape, act_shape, rng=rng)
+        if sac_buffer is None:
+            return new_buffer
+        (
+            obs,
+            action,
+            next_obs,
+            reward,
+            terminated,
+            truncated,
+        ) = sac_buffer.get_all().astuple()
+        new_buffer.add_batch(obs, action, next_obs, reward, terminated, truncated)
+        return new_buffer
+    return sac_buffer
+
+
+def train(
+    env: gym.Env,
+    test_env: gym.Env,
+    termination_fn: mbrl.types.TermFnType,
+    cfg: omegaconf.DictConfig,
+    silent: bool = False,
+    work_dir: Optional[str] = None,
+    reward_fn: Optional[mbrl.types.RewardFnType] = None,
+) -> np.float32:
+    # ------------------- Initialization -------------------
+    debug_mode = cfg.get("debug_mode", False)
+
+    obs_shape = env.observation_space.shape
+    act_shape = env.action_space.shape
+
+    mbrl.planning.complete_agent_cfg(env, cfg.algorithm.agent)
+    agent = SACAgent(
+        cast(pytorch_sac_pranz24.SAC, hydra.utils.instantiate(cfg.algorithm.agent))
+    )
+
+    work_dir = work_dir or os.getcwd()
+    # enable_back_compatible to use pytorch_sac agent
+    logger = mbrl.util.Logger(work_dir, enable_back_compatible=True, use_wandb=cfg.use_wandb)
+    logger.register_group(
+        mbrl.constants.RESULTS_LOG_NAME,
+        MBPO_LOG_FORMAT,
+        color="green",
+        dump_frequency=1,
+    )
+    custom_log_handler = CustomLogHandler(logger)
+    save_video = cfg.get("save_video", False)
+    video_recorder = VideoRecorder(work_dir if save_video else None)
+
+    rng = np.random.default_rng(seed=cfg.seed)
+    torch_generator = torch.Generator(device=cfg.device)
+    if cfg.seed is not None:
+        torch_generator.manual_seed(cfg.seed)
+
+    # -------------- Create initial overrides. dataset --------------
+    dynamics_model = mbrl.util.common.create_one_dim_tr_model(cfg, obs_shape, act_shape)
+    use_double_dtype = cfg.algorithm.get("normalize_double_precision", False)
+    dtype = np.double if use_double_dtype else np.float32
+    replay_buffer = mbrl.util.common.create_replay_buffer(
+        cfg,
+        obs_shape,
+        act_shape,
+        rng=rng,
+        obs_type=dtype,
+        action_type=dtype,
+        reward_type=dtype,
+    )
+    random_explore = cfg.algorithm.random_initial_explore
+    if debug_mode:
+        print("Collecting initial trajectories")
+    mbrl.util.common.rollout_agent_trajectories(
+        env,
+        cfg.algorithm.initial_exploration_steps,
+        mbrl.planning.RandomAgent(env) if random_explore else agent,
+        {} if random_explore else {"sample": True, "batched": False},
+        replay_buffer=replay_buffer,
+    )
+
+    # ---------------------------------------------------------
+    # --------------------- Training Loop ---------------------
+    rollout_batch_size = (
+        cfg.overrides.effective_model_rollouts_per_step * cfg.algorithm.freq_train_model
+    )
+    trains_per_epoch = int(
+        np.ceil(cfg.overrides.epoch_length / cfg.overrides.freq_train_model)
+    )
+    updates_made = 0
+    env_steps = 0
+    model_env = mbrl.models.ModelEnv(
+        env, dynamics_model, termination_fn, reward_fn, generator=torch_generator
+    )
+    model_trainer = mbrl.models.ModelTrainer(
+        dynamics_model,
+        optim_lr=cfg.overrides.model_lr,
+        weight_decay=cfg.overrides.model_wd,
+        logger=None if silent else logger,
+    )
+    best_eval_reward = -np.inf
+    epoch = 0
+    sac_buffer = None
+    while env_steps < cfg.overrides.num_steps:
+        rollout_length = int(
+            mbrl.util.math.truncated_linear(
+                *(cfg.overrides.rollout_schedule + [epoch + 1])
+            )
+        )
+        sac_buffer_capacity = rollout_length * rollout_batch_size * trains_per_epoch
+        sac_buffer_capacity *= cfg.overrides.num_epochs_to_retain_sac_buffer
+        sac_buffer = maybe_replace_sac_buffer(
+            sac_buffer, obs_shape, act_shape, sac_buffer_capacity, cfg.seed
+        )
+        obs = None
+        terminated = False
+        truncated = False
+        obs_list, rew_list, success_list, action_list = [], [], [], []
+        for steps_epoch in range(cfg.overrides.epoch_length):
+            if steps_epoch == 0 or terminated or truncated:
+                steps_epoch = 0
+                obs, _ = env.reset()
+                terminated = False
+                truncated = False
+                # stats
+                if env_steps > 0:
+                    success_list.append(1*(reward == 0))
+            # --- Doing env step and adding to model dataset ---
+            (
+                next_obs,
+                reward,
+                terminated,
+                truncated,
+                _,
+            ) = mbrl.util.common.step_env_and_add_to_buffer(
+                env, obs, agent, {}, replay_buffer
+            )
+            assert not terminated
+            # stats
+            obs_list.append(obs.copy()), rew_list.append(reward), action_list.append(0) # TODO: move to inside step_env() function
+            
+            # --------------- Model Training -----------------
+            if ((env_steps + 1) % cfg.overrides.freq_train_model == 0) and cfg.overrides.do_imag_training:
+                if debug_mode:
+                    print("Training dynamics model")
+                mbrl.util.common.train_model_and_save_model_and_data(
+                    dynamics_model,
+                    model_trainer,
+                    cfg.overrides,
+                    replay_buffer,
+                    work_dir=work_dir,
+                    env_steps=env_steps,
+                )
+                
+                if debug_mode:
+                    print("Collecting imaginary trajectories")
+                # --------- Rollout new model and store imagined trajectories --------
+                # Batch all rollouts for the next freq_train_model steps together
+                rollout_model_and_populate_sac_buffer(
+                    model_env,
+                    replay_buffer,
+                    agent,
+                    sac_buffer,
+                    cfg.algorithm.sac_samples_action,
+                    rollout_length,
+                    rollout_batch_size,
+                    env_steps,
+                    custom_log_handler
+                )
+                
+                if debug_mode:
+                    print(
+                        f"Epoch: {epoch}. "
+                        f"SAC buffer size: {len(sac_buffer)}. "
+                        f"Rollout length: {rollout_length}. "
+                        f"Steps: {env_steps}"
+                    )
+
+            # --------------- Agent Training -----------------
+            for _ in range(cfg.overrides.num_sac_updates_per_step):
+                use_real_data = rng.random() < cfg.algorithm.real_data_ratio
+                if not cfg.overrides.do_imag_training:
+                    assert use_real_data
+                which_buffer = replay_buffer if use_real_data else sac_buffer
+                if (env_steps + 1) % cfg.overrides.sac_updates_every_steps != 0 or len(
+                    which_buffer
+                ) < cfg.overrides.sac_batch_size:
+                    break  # only update every once in a while
+                
+                if debug_mode:
+                    print("Training SAC agent")
+                agent.sac_agent.update_parameters(
+                    which_buffer,
+                    cfg.overrides.sac_batch_size,
+                    updates_made,
+                    logger,
+                    reverse_mask=True,
+                )
+                logger.log("train/env_step", env_steps, updates_made)
+                logger.log("train/real_buffer_size", len(replay_buffer), updates_made)
+                logger.log("train/sac_buffer_size", len(sac_buffer), updates_made)
+                updates_made += 1
+                if not silent and updates_made % cfg.log_frequency_agent == 0:
+                    logger.dump(updates_made, save=True)
+
+            # ------ Epoch ended (evaluate and save model) ------
+            if (env_steps + 1) % cfg.overrides.epoch_length == 0:
+                if debug_mode:
+                    print("Evaluating agent")
+                avg_reward, avg_success = evaluate(
+                    test_env, agent, cfg.algorithm.num_eval_episodes, video_recorder
+                )
+                logger.log_data(
+                    mbrl.constants.RESULTS_LOG_NAME,
+                    {
+                        "epoch": epoch,
+                        "env_step": env_steps,
+                        "episode_reward": avg_reward,
+                        "episode_success": avg_success,
+                        "rollout_length": rollout_length,
+                    },
+                )
+                if avg_reward > best_eval_reward:
+                    video_recorder.save(f"{epoch}.mp4")
+                    best_eval_reward = avg_reward
+                    agent.sac_agent.save_checkpoint(
+                        ckpt_path=os.path.join(work_dir, "sac.pth")
+                    )
+                epoch += 1
+
+            env_steps += 1
+            obs = next_obs
+
+        # stats
+        custom_log_handler.log_rollout(env_steps, obs_list, rew_list, action_list, success_list=success_list, type="rollout_real")
+
+    return np.float32(best_eval_reward)
---
+++ mbrl/examples/main_custom.py
@@ -0,0 +1,99 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+import hydra
+import numpy as np
+import omegaconf
+import torch
+
+import mbrl.algorithms.mbpo_custom as mbpo_custom
+import mbrl.env
+
+from llm_curriculum_algo.env_wrappers import make_env as make_env_inner
+import wandb
+
+class CustomRewardFn:
+    def __init__(self, env):
+        self.env = env
+
+    def custom_reward_fn(self, act: torch.Tensor, next_obs: torch.Tensor) -> torch.Tensor:
+        assert len(next_obs.shape) == len(act.shape) == 2
+
+        rewards = torch.zeros(act.shape[0], 1)
+        for i in range(next_obs.shape[0]):
+            reward = self.env.agent_conductor.get_active_task_reward(next_obs[i].numpy(force=True))
+            rewards[i] = reward
+
+        return torch.tensor(rewards)
+
+def setup_wandb(cfg):
+    wandb.init(
+        entity='robertmccarthy11',
+        project='llm-curriculum',
+        group='temp',
+        name='temp',
+        job_type='training',
+        # config=vargs,
+        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+        monitor_gym=False,  # auto-upload the videos of agents playing the game
+        save_code=False,  # optional
+        )
+
+def temp_env_test(env, reward_fn):
+    obs, _ = env.reset()
+    for i in range(50):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        reward_fn_val = reward_fn(torch.tensor(action).unsqueeze(0), torch.tensor(obs).unsqueeze(0))
+        print(f"reward: {reward}, reward_fn: {reward_fn_val}")
+        print(f"i={i}: terminated: {terminated}, truncated: {truncated}")
+        input()
+    obs, _ = env.reset()
+    for i in range(50):
+        obs, reward, terminated, truncated, info = env.step(env.get_oracle_action(obs))
+        reward_fn_val = reward_fn(torch.tensor(action).unsqueeze(0), torch.tensor(obs).unsqueeze(0))
+        print(f"reward: {reward}, reward_fn: {reward_fn_val}")
+        print(f"i={i}: terminated: {terminated}, truncated: {truncated}")
+        input()
+    import pdb; pdb.set_trace()
+
+def make_env(cfg):
+    # TODO: get hparams from cfg?
+    env = make_env_inner(
+            manual_decompose_p=1,
+            dense_rew_lowest=False,
+            dense_rew_tasks=['move_gripper_to_cube'], #
+            use_language_goals=False,
+            render_mode='rgb_array',
+            max_ep_len=50,
+            single_task_names=['move_gripper_to_cube'], #
+            high_level_task_names=['move_cube_to_target'],
+            contained_sequence=False,
+            state_obs_only=True,
+            curriculum_manager_cls=None,
+            old_gym=False,
+            ) # TODO: add args to config
+    
+    assert len(env.agent_conductor.single_task_names) == 1, "Only setup for 1 task currently"
+    term_fn = mbrl.env.termination_fns.no_termination
+    if cfg.algorithm.learned_rewards:
+        reward_fn = None
+    else:
+        reward_fn = CustomRewardFn(env).custom_reward_fn
+    # temp_env_test(env, reward_fn)
+    return env, term_fn, reward_fn
+
+@hydra.main(config_path="conf", config_name="main_custom")
+def run(cfg: omegaconf.DictConfig):
+    if cfg.use_wandb:
+        setup_wandb(cfg)
+    env, term_fn, reward_fn = make_env(cfg)
+    np.random.seed(cfg.seed)
+    torch.manual_seed(cfg.seed)
+    test_env, *_ = make_env(cfg)
+    return mbpo_custom.train(env, test_env, term_fn, cfg, reward_fn=reward_fn)
+
+
+if __name__ == "__main__":
+    run()
